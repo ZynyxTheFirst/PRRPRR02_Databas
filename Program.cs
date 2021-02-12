12780032529fd27b9bd4c5779c84ae23765b0e9f
Robin Westling <rejjkill.redunzl@gmail.com>
2021-02-12 13:23:54 +0100

    12/02/21-2

--- Program.cs
+++ Program.cs
@@ -17,10 +17,11 @@ class Program
         
     }
 
-    //skriv om
+    //Fix at some point
+    /*
     void Load()
     {
-        var list = SqliteDataAccess.LoadPeople();
+        var list = SqliteDataAccess.LoadElever();
         foreach (EleverModel p in list)
             Console.WriteLine($"id: {p.Elev_Personnummer}, name: {p.Namn} {p.Adress}");
     }
@@ -36,19 +37,27 @@ class Program
         Console.WriteLine("Last name: \n>>");
         string lastName = Console.ReadLine();
 
-        SqliteDataAccess.SavePerson(new EleverModel() { Namn = name, Adress = lastName });
+        SqliteDataAccess.AddElev(new EleverModel() { Namn = name, Adress = lastName });
     }
+    */
 }
 
 class SqliteDataAccess
 {
-    public static void AddElev(EleverModel elev)
+    //Untested should work, might not
+    //Fix "intermidiary" table generation (kursregistrering etc.)
+
+    /// <summary>
+    /// Finished with the exception of this tying in with other tables and intermidiary table generation
+    /// </summary>
+    /// <param name="Elever"></param>
+    #region Elever
+    public static void AddElev(EleverModel Elev)
     {
         using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
-        cnn.Execute($"INSERT INTO Elever VALUES (@Elev_Personnummer, @Namn, @Adress, @Epost, @Telefonnummer, @Klass_Namn)", elev);
+        cnn.Execute($"INSERT INTO Elever VALUES (@Elev_Personnummer, @Namn, @Adress, @Epost, @Telefonnummer, @Klass_Namn)", Elev);
     }
-
-    public static List<EleverModel> LoadElever()
+    public static List<EleverModel> LoadEleverList()
     {
         using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
         var output = cnn.Query<EleverModel>("SELECT * FROM Elever", new DynamicParameters());
@@ -57,19 +66,123 @@ class SqliteDataAccess
     public static EleverModel LoadElev(string ElevPersonnummer)
     {
         using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
-        var output = cnn.Query<EleverModel>("SELECT * FROM Elever WHERE Elev_Personnummer ='" + ElevPersonnummer + "'", new DynamicParameters());
+        var output = cnn.Query<EleverModel>("SELECT * FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'", new DynamicParameters());
         return output.ToList()[0];
     }
-    public static void SaveElev(EleverModel person)
+    public static void RemoveElev(string ElevPersonnummer)
     {
         using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
-        cnn.Execute("INSERT INTO people (first_name, last_name) VALUES (@FirstName, @LastName)", person);
+        cnn.Query<EleverModel>("DELETE FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'");
     }
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Vårdnadshavare"></param>
+    #region Vårdnadshavare
+    public static void AddVårdnadshavare(VårdnadshavareModel Vårdnadshavare)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Execute($"INSERT INTO Vårdnadshavare VALUES (@Vårdnadshavare_Personnummer, @Namn, @Adress, @Epost, @Telefonnummer)", Vårdnadshavare);
+    }
+    public static List<VårdnadshavareModel> LoadVårdnadshavareList()
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<VårdnadshavareModel>("SELECT * FROM Vårdnadshavare", new DynamicParameters());
+        return output.ToList();
+    }
+    public static VårdnadshavareModel LoadVårdnadshavare(string VårdnadshavarePersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<VårdnadshavareModel>("SELECT * FROM Vårdnadshavare WHERE Vårdnadshavare_Personnummer='" + VårdnadshavarePersonnummer + "'", new DynamicParameters());
+        return output.ToList()[0];
+    }
+    public static void RemoveVårdnadshavare(string VårdnadshavarePersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Query<VårdnadshavareModel>("DELETE FROM Vårdnadshavare WHERE Vårdnadshavare_Personnummer='" + VårdnadshavarePersonnummer + "'");
+    }
+    #endregion
+
+    #region Lärare
+    public static void AddLärare(LärareModel lärare)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Execute($"INSERT INTO Lärare VALUES (@Lärare_Personnummer, @Namn, @Adress, @Telefonnummer, @Epost)", lärare);
+    }
+    public static List<LärareModel> LoadLärareList()
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<LärareModel>("SELECT * FROM Lärare", new DynamicParameters());
+        return output.ToList();
+    }
+    public static LärareModel LoadLärare(string LärarePersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<LärareModel>("SELECT * FROM Lärare WHERE Lärare_Personnummer='" + LärarePersonnummer + "'", new DynamicParameters());
+        return output.ToList()[0];
+    }
+    public static void RemoveLärare(string LärarePersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Query<LärareModel>("DELETE FROM Lärare WHERE Lärare_Personnummer='" + LärarePersonnummer + "'");
+    }
+    #endregion
+
+    #region Kurser
+    public static void AddKurs(EleverModel elev)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Execute($"INSERT INTO Elever VALUES (@Elev_Personnummer, @Namn, @Adress, @Epost, @Telefonnummer, @Klass_Namn)", elev);
+    }
+    public static List<EleverModel> LoadKurserList()
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<EleverModel>("SELECT * FROM Elever", new DynamicParameters());
+        return output.ToList();
+    }
+    public static EleverModel LoadKurs(string ElevPersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<EleverModel>("SELECT * FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'", new DynamicParameters());
+        return output.ToList()[0];
+    }
+    public static void RemoveKurs(string ElevPersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Query<EleverModel>("DELETE FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'");
+    }
+    #endregion
+
+    #region Klasser
+    public static void AddKlass(EleverModel elev)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Execute($"INSERT INTO Elever VALUES (@Elev_Personnummer, @Namn, @Adress, @Epost, @Telefonnummer, @Klass_Namn)", elev);
+    }
+    public static List<EleverModel> LoadKlasserList()
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<EleverModel>("SELECT * FROM Elever", new DynamicParameters());
+        return output.ToList();
+    }
+    public static EleverModel LoadKlass(string ElevPersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        var output = cnn.Query<EleverModel>("SELECT * FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'", new DynamicParameters());
+        return output.ToList()[0];
+    }
+    public static void RemoveKlass(string ElevPersonnummer)
+    {
+        using IDbConnection cnn = new SQLiteConnection(LoadConnectionString());
+        cnn.Query<EleverModel>("DELETE FROM Elever WHERE Elev_Personnummer='" + ElevPersonnummer + "'");
+    }
+    #endregion
 
     //viktig rör ej
     private static string LoadConnectionString(string id = "Default")
     {
-        //gå in i App.config och ge vår ConnectionString till oss!
         return ConfigurationManager.ConnectionStrings[id].ConnectionString;
     }
 }
